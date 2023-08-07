a5977c34159030daaea0e147925feedf3df22543
syed abdul hakim <s.abdulhakim10@gmail.com>
2023-08-08 02:32:40 +0600

    maping and alignment practice

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,5 +1,6 @@
 import 'package:adv_basics/action_button.dart';
 import 'package:flutter/material.dart';
+import 'package:adv_basics/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,23 +12,32 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+  final currentQuestions = questions[0];
   @override
   Widget build(context) {
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('The Question....'),
-          const SizedBox(
-            height: 30,
-          ),
-          ActionButton(actionText: 'Answer', onTap: () {}),
-          const SizedBox(height: 8),
-          ActionButton(actionText: 'Answer', onTap: () {}),
-          const SizedBox(height: 8),
-          ActionButton(actionText: 'Answer', onTap: () {}),
-        ],
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestions.text,
+              style: const TextStyle(color: Colors.white, fontSize: 16),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestions.answer.map(
+              (answer) {
+                return ActionButton(actionText: answer, onTap: () {});
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
